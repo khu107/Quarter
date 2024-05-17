86c244f4162c1358f855d8df5b017ea9741fc874
metablog <khu10735@naver.com>
2024-05-18 02:40:08 +0900

    feat: create getAllMembersByAdmin and updateMemberByAdmin  GraphQL Apis

--- apps/nestar-api/src/components/auth/auth.module.ts
+++ apps/nestar-api/src/components/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-// import { HttpModule } from '@nestjs/axios';
+import { HttpModule } from '@nestjs/axios';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
 	imports: [
-		// HttpModule,
+		HttpModule,
 		JwtModule.register({
 			secret: `${process.env.SECRET_TOKEN}`,
 			signOptions: { expiresIn: '30d' },
