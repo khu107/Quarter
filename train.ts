27160380970fdb1c19bd8532659da1c094632d6e
metablog <khu10735@naver.com>
2024-05-21 04:26:45 +0900

    fix: ZO

--- train.ts
+++ train.ts
@@ -1,20 +1,19 @@
-// ZM-TASK:
+// ZO-TASK:
 
-function rotateArray(arr: number[], index: number): number[] {
-	const rotated: number[] = [];
-	const len: number = arr.length;
-	index = (index - 1) % len;
+function areParenthesesBalanced(arr: string): boolean {
+	let ochish: number = 0;
+	let yopish: number = 0;
 
-	for (let i = len - index; i < len; i++) {
-		rotated.push(arr[i]);
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === '(') {
+			ochish += 1;
+		} else if (arr[i] === ')') {
+			yopish += 1;
+		}
 	}
+	console.log(ochish, yopish);
 
-	for (let i = 0; i < len - index; i++) {
-		rotated.push(arr[i]);
-	}
-
-	return rotated;
+	return ochish === yopish ? true : false;
 }
 
-const result: number[] = rotateArray([1, 2, 3, 4, 5, 6], 3);
-console.log(result); // [5, 6, 1, 2, 3, 4]
+console.log(areParenthesesBalanced('string()ichida(qavslar)soni()balansda'));
