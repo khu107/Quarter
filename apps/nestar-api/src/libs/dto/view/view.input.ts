b27881154e59d31c5d8d4dfff5ac1ea887851693
metablog <khu10735@naver.com>
2024-05-25 02:44:22 +0900

    feat: develop getProperty business logic

--- apps/nestar-api/src/libs/dto/view/view.input.ts
+++ apps/nestar-api/src/libs/dto/view/view.input.ts
@@ -1,16 +1,17 @@
 import { Field, InputType } from '@nestjs/graphql';
 import { IsNotEmpty } from 'class-validator';
 import { ViewGroup } from '../../enums/view.enum';
+import { ObjectId } from 'mongoose';
 
 @InputType()
 export class ViewInput {
 	@IsNotEmpty()
 	@Field(() => String)
-	memberId: Object;
+	memberId: ObjectId;
 
 	@IsNotEmpty()
 	@Field(() => String)
-	viewRefId: Object;
+	viewRefId: ObjectId;
 
 	@IsNotEmpty()
 	@Field(() => ViewGroup)
