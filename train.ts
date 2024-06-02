b863ac15955390e58fa2d2086575a0dac78ae0ac
metablog <khu10735@naver.com>
2024-06-03 03:33:30 +0900

    fix: ZU

--- train.ts
+++ train.ts
@@ -1,24 +1,23 @@
-// ZT-TASK:
+// ZU-TASK:
 
-function firstUniqueCharIndex(s: string): number {
-	const charCount: { [key: string]: number } = {};
+function sumOfUnique(nums: number[]): number {
+	const ob: Record<number, number> = {};
 
-	for (let i = 0; i < s.length; i++) {
-		const char = s[i];
-		if (charCount[char] === undefined) {
-			charCount[char] = 1;
+	nums.forEach((n) => {
+		if (ob[n]) {
+			ob[n] += 1;
 		} else {
-			charCount[char]++;
+			ob[n] = 1;
+		}
+	});
+
+	let sum = 0;
+	for (let key in ob) {
+		if (ob[key] === 1) {
+			sum += Number(key);
 		}
 	}
-
-	for (let i = 0; i < s.length; i++) {
-		if (charCount[s[i]] === 1) {
-			return i;
-		}
-	}
-
-	return -1;
+	return sum;
 }
 
-console.log(firstUniqueCharIndex('stamp'));
+console.log(sumOfUnique([1, 2, 3, 2]));
